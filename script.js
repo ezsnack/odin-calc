0b8f5f6a48f1c88e509fa396e58f33136cedacc7
ezsnack <204544689+ezsnack@users.noreply.github.com>
2025-12-29 07:43:23 +0900

    Add keyboard support

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// variables
 let displayString = "";
 const MAXLEN = 8; //maximum input operand length
 let x = NaN; //first operand
@@ -8,6 +9,7 @@ let operatorChosen = false;
 let decimalInputActive = false;
 let justCalculated = false;
 
+// document elements
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".op");
 const result = document.querySelector("#calculate");
@@ -21,42 +23,13 @@ const sub = document.querySelector("#sub");
 const mul = document.querySelector("#mul");
 const div = document.querySelector("#div");
 
+// event listeners
 for (let number of numbers) {
-  const inputNumber = number.textContent;
-  number.addEventListener("click", () => {
-    if (justCalculated) {
-      console.log("cannot add numbers to the result. press an operator to calculate using the current result as first value, or press 'C' to clear");
-    } else if (displayString.length < MAXLEN) {
-      displayString += inputNumber;
-      updateDisplay();
-    } else {
-      console.log("digit limit reached. number will not be updated")
-    }
-  });
+  const input = number.textContent;
+  number.addEventListener("click", () => inputNumber(input));
 }
-
 for (let operator of operators) {
-  operator.addEventListener("click", () => {
-    if (justCalculated) {
-      justCalculated = false;
-      operatorChosen = true;
-    } else if (firstNumberPresent) {
-      y = parseFloat(displayString);
-      calculateResult();
-      operatorChosen = true;
-      justCalculated = false; // so a new number can be input
-    } else {
-      x = parseFloat(displayString);
-      displayString = "";
-      operatorChosen = true;
-      firstNumberPresent = true;
-      decimalInputActive = false;
-      if (Number.isNaN(x)) {
-        console.log("first operand has not been input or is invalid. resetting");
-        clear();
-      }
-    }
-  });
+  operator.addEventListener("click", () => addOperator);
 }
 add.addEventListener("click", () => method = addition);
 sub.addEventListener("click", () => method = subtraction);
@@ -67,6 +40,39 @@ clr.addEventListener("click", clear);
 decimal.addEventListener("click", decimalInput);
 back.addEventListener("click", backspace);
 
+// functions
+function inputNumber(number) {
+  if (justCalculated) {
+    console.log("cannot add numbers to the result. press an operator to calculate using the current result as first value, or press 'C' to clear");
+  } else if (displayString.length < MAXLEN) {
+    displayString += number;
+    updateDisplay();
+  } else {
+    console.log("digit limit reached. number will not be updated")
+  }
+}
+
+function addOperator() {
+  if (justCalculated) {
+    justCalculated = false;
+    operatorChosen = true;
+  } else if (firstNumberPresent) {
+    y = parseFloat(displayString);
+    calculateResult();
+    operatorChosen = true;
+    justCalculated = false; // so a new number can be input
+  } else {
+    x = parseFloat(displayString);
+    displayString = "";
+    operatorChosen = true;
+    firstNumberPresent = true;
+    decimalInputActive = false;
+    if (Number.isNaN(x)) {
+      console.log("first operand has not been input or is invalid. resetting");
+      clear();
+    }
+  }
+}
 
 function decimalInput() {
   if (decimalInputActive) {
@@ -155,3 +161,73 @@ function division(x, y) {
 
 function nothing() {
 }
+
+// keyboard support
+document.addEventListener("keydown", event => {
+  switch (event.key) {
+    case "1":
+      inputNumber("1");
+      break;
+    case "2":
+      inputNumber("2");
+      break;
+    case "3":
+      inputNumber("3");
+      break;
+    case "4":
+      inputNumber("4");
+      break;
+    case "5":
+      inputNumber("5");
+      break;
+    case "6":
+      inputNumber("6");
+      break;
+    case "7":
+      inputNumber("7");
+      break;
+    case "8":
+      inputNumber("8");
+      break;
+    case "9":
+      inputNumber("9");
+      break;
+    case "0":
+      inputNumber("0");
+      break;
+    case ".":
+      decimalInput();
+      break;
+    case "+":
+      addOperator();
+      method = addition;
+      break;
+    case "-":
+      addOperator();
+      method = subtraction;
+      break;
+    case "*":
+      addOperator();
+      method = multiplication;
+      break;
+    case "/":
+    case ":":
+      addOperator();
+      method = division;
+      break;
+    case "Enter":
+    case "=":
+      calculateResult();
+      break;
+    case "Backspace":
+      backspace();
+      break;
+    case "c":
+    case "C":
+      clear();
+      break;
+    default:
+      console.log("no operations bound for this key");
+      break;
+  }
+});
